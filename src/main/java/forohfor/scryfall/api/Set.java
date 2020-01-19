b65c58c4d710abc80b97a68390a7eb26681e8536
ForOhForError <16399039+ForOhForError@users.noreply.github.com>
2020-01-19 15:06:27 -0500

    Same treatment for sets

--- src/main/java/forohfor/scryfall/api/Set.java
+++ src/main/java/forohfor/scryfall/api/Set.java
@@ -1,6 +1,7 @@
 package forohfor.scryfall.api;
 
 import java.util.Date;
+import java.util.UUID;
 
 import org.json.simple.JSONObject;
 
@@ -10,109 +11,147 @@ import org.json.simple.JSONObject;
  */
 
 public class Set {
-	private String id;
-	private String code;
-	private String name;
-	private String searchUri;
-	private String setType;
-	private Boolean digitalOnly;
-	private Boolean hasFoils;
-	private Date releasedAt;
-	private String blockCode;
-	private String blockName;
-	private int cardCount;
-	private String setIconURI;
-	private String parentSetCode;
 
+	private JSONObject json;
 
 	/**
 	 * Builds a Set object from JSON data.
 	 * @param setData The JSON object representing the set.
 	 */
-	public Set(JSONObject setData) {
-		id = JSONUtil.getStringData(setData,"id");
-		code = JSONUtil.getStringData(setData,"code");
-		name = JSONUtil.getStringData(setData,"name");
-		searchUri = JSONUtil.getStringData(setData,"search_uri");
-		setType = JSONUtil.getStringData(setData,"set_type");
-		digitalOnly = JSONUtil.getBoolData(setData,"digital");
-		hasFoils = JSONUtil.getBoolData(setData,"foil");
-		blockCode = JSONUtil.getStringData(setData,"block_code");
-		blockName = JSONUtil.getStringData(setData,"block");
-		cardCount = JSONUtil.getIntData(setData,"card_count");
-		releasedAt = JSONUtil.getIsoDateData(setData,"released_at");
-		setIconURI = JSONUtil.getStringData(setData, "icon_svg_uri");
-		parentSetCode = JSONUtil.getStringData(setData, "parent_set_code");
+	public Set(JSONObject setData) 
+	{
+		json = new JSONObject(setData);
 	}
 
 	/**
-	 * @return The set code for this set.
+	 * @return A UUID for this set on Scryfall that will not change. 
 	 */
-	public String getCode() {
-		return code;
+	public UUID getId() 
+	{
+		return UUID.fromString(JSONUtil.getStringData(json, "id"));
 	}
 
 	/**
-	 * @return The name of this set.
+	 * @return The unique three to five-letter code for this set. 
 	 */
-	public String getName() {
-		return name;
+	public String getCode() 
+	{
+		return JSONUtil.getStringData(json, "code");
 	}
 
 	/**
-	 * @return The URI containing the data of all cards in this set
+	 * @return The unique code for this set on MTGO, which may differ from the regular code. 
 	 */
-	public String getSearchUri() {
-		return searchUri;
+	public String getMtgoCode() 
+	{
+		return JSONUtil.getStringData(json, "mtgo_code");
 	}
 
 	/**
-	 * @return The type of the set. See scryfall's API documentation.
+	 * @return This set’s ID on TCGplayer’s API, also known as the groupId. 
+	 */
+	public String getTCGplayerID()
+	{
+		return JSONUtil.getStringData(json, "tcgplayer_id");
+	}
+
+	/**
+	 * @return The English name of this set.
+	 */
+	public String getName() 
+	{
+		return JSONUtil.getStringData(json, "name");
+	}
+
+	/**
+	 * @return A computer-readable classification for this set. 
+	 * See scryfall's API documentation.
 	 */
 	public String getSetType() {
-		return setType;
+		return JSONUtil.getStringData(json, "set_type");
 	}
 
 	/**
-	 * @return True if this set was only released on MTGO
-	 */
-	public Boolean isDigitalOnly() {
-		return digitalOnly;
-	}
-
-	/**
-	 * @return True if this set contains foil(s)
-	 */
-	public Boolean hasFoils() {
-		return hasFoils;
-	}
-
-	/**
-	 * @return The date this set was released at
+	 * @return The date the set was released or the first card was printed in the set 
+	 * (in GMT-8 Pacific time). 
 	 */
 	public Date getReleasedAt() {
-		return releasedAt;
+		return JSONUtil.getIsoDateData(json, "released_at");
 	}
 
 	/**
-	 * @return the blockCode
+	 * @return The block code for this set, if any. 
 	 */
 	public String getBlockCode() {
-		return blockCode;
+		return JSONUtil.getStringData(json, "block_code");
+	}
+
+	/**
+	 * @return The block or group name code for this set, if any. 
+	 */
+	public String getBlockName() {
+		return JSONUtil.getStringData(json, "block");
 	}
 	
 	/**
-	 * @return the block name
+	 * @return The set code for the parent set, if any. 
+	 * promo and token sets often have a parent set. 
 	 */
-	public String getBlockName() {
-		return blockName;
+	public String getParentSetCode() {
+		return JSONUtil.getStringData(json, "parent_set_code");
 	}
 
 	/**
 	 * @return the cardCount
 	 */
 	public int getCardCount() {
-		return cardCount;
+		return JSONUtil.getIntData(json, "card_count");
+	}
+
+	/**
+	 * @return True if this set was only released on Magic Online. 
+	 */
+	public boolean isDigital() {
+		return JSONUtil.getBoolData(json, "digital");
+	}
+
+	/**
+	 * @return True if this set contains only foil cards. 
+	 */
+	public boolean isFoilOnly() {
+		return JSONUtil.getBoolData(json, "foil_only");
+	}
+
+	/**
+	 * @return A link to this set’s permapage on Scryfall’s website. 
+	 */
+	public String getScryfallURI() {
+		return JSONUtil.getStringData(json, "scryfall_uri");
+	}
+
+	/**
+	 * @return A link to this set object on Scryfall’s API. 
+	 */
+	public String getURI() {
+		return JSONUtil.getStringData(json, "uri");
+	}
+
+
+	/**
+	 * @return A Scryfall API URI that you can request to begin paginating over the cards in this set. 
+	 */
+	public String getSearchURI() {
+		return JSONUtil.getStringData(json, "search_uri");
+	}
+
+	/**
+	 * @return A URI to an SVG file for this set’s icon on Scryfall’s CDN. 
+	 * Hotlinking this image isn’t recommended, because it may change slightly over time. 
+	 * You should download it and use it locally for your particular user interface needs. 
+	 */
+	public String getSetIconURI()
+	{
+		return JSONUtil.getStringData(json, "icon_svg_uri");
 	}
 
 	/* (non-Javadoc)
@@ -120,28 +159,6 @@ public class Set {
 	 */
 	@Override
 	public String toString() {
-		return name+" ("+code+")";
+		return getName()+" ("+getCode()+")";
 	}
-
-	/**
-	 * @return the set icon's URI
-	 */
-	public String getSetIconURI()
-	{
-		return setIconURI;
-	}
-
-	/**
-	 * @return the UUID of this set
-	 */
-	public String getId() {
-		return id;
-	}
-
-	/**
-	 * @return the code of this set's parent set
-	 */
-	public String getParentSetCode() {
-		return parentSetCode;
-	}	
 }
